7f4937de609fb393a1f629b8fa82cb8b91d177da
LP-user1 <akashbhuvi3@gmail.com>
2023-01-24 21:00:12 +0530

    Feature : Added state wise search

--- assests/js/main.js
+++ assests/js/main.js
@@ -8,57 +8,79 @@ const type = document.querySelector("#type");
 const dist = document.querySelector("#dist");
 const btn = document.querySelector("button");
 const spinner = document.querySelector(".spin");
+const alertSec = document.getElementById("alert-sec");
+const tableEl = document.querySelector(".t-data");
 
 inputEl.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
-    findRecord();
+    findSeperateRecord();
   }
 });
-btn.addEventListener("click", findRecord);
+btn.addEventListener("click", findSeperateRecord);
 
-function findRecord() {
+function findSeperateRecord() {
   if (inputEl.value.length > 3) {
-    errorEl.parentElement.classList.add("d-none");
     spinnerActive();
-    setTimeout(searchRec(inputEl.value), 1000);
+    searchRec(inputEl.value);
+  } else if (inputEl.value.length === 2) {
+    spinnerActive();
+    searchRec(inputEl.value);
   } else {
-    errorEl.parentElement.classList.remove("d-none");
-    errorEl.parentElement.classList.remove("alert-danger");
-    errorEl.parentElement.classList.add("alert-info");
-    queryEl.parentElement.parentElement.classList.add("d-none");
-    errorEl.innerText = "Enter minimum 4 characters!! ex: TN74.";
+    alertMsg("INFO ", "info", "Enter 2 or 4 characters | Ex:TN or TN74");
   }
 }
 
 async function getJson() {
   const response = await fetch("./assests/json/data.json");
-  spinnerActive();
   return await response.json();
 }
 
 const searchRec = async (value) => {
   const jsonData = await getJson();
 
-  const filterVal = jsonData.find(
-    (record) => record.code === value.toUpperCase()
+  if (inputEl.value.length == 2) {
+    const filterVal = jsonData.filter((record) => {
+      return record.code.slice(0, 2) === value.toUpperCase();
+    });
 
-    // Better search with arrow function
-  );
-
-  if (!filterVal) {
-    errorEl.parentElement.classList.remove("d-none");
-    errorEl.parentElement.classList.add("alert-danger");
-    errorEl.innerText = "Result not found !Try another RTO code.";
-    queryEl.parentElement.parentElement.classList.add("d-none");
+    const filterExecution = () => {
+      spinnerActive();
+      if (!filterVal) {
+        alertMsg("Not Found ", "danger", "Try different State code");
+      } else {
+        queryEl.parentElement.parentElement.classList.remove("d-none");
+        document.querySelector(".singleData").classList.add("d-none");
+        document.querySelector(".allData").classList.remove("d-none");
+        for (i = 0; i < filterVal.length; i++) {
+          const tableVal = document.createElement("tr");
+          tableVal.innerHTML += `
+              <td>${filterVal[i].state}</td>
+              <td>${filterVal[i].code}</td>
+              <td>${filterVal[i].location}</td>`;
+          tableEl.appendChild(tableVal);
+        }
+      }
+    };
+    filterExecution();
   } else {
-    errorEl.parentElement.classList.add("d-none");
-    queryEl.parentElement.parentElement.classList.remove("d-none");
-    queryEl.innerText = inputEl.value;
-    id.innerText = filterVal.id;
-    code.innerText = filterVal.code;
-    loc.innerText = filterVal.location;
-    type.innerText = filterVal.type;
-    dist.innerText = filterVal.district;
+    const findVal = jsonData.find(
+      (record) => record.code === value.toUpperCase()
+    );
+
+    spinnerActive();
+    if (!findVal) {
+      alertMsg("Not Found ", "danger", "Try different RTO code");
+    } else {
+      queryEl.parentElement.parentElement.classList.remove("d-none");
+      document.querySelector(".allData").classList.add("d-none");
+      document.querySelector(".singleData").classList.remove("d-none");
+      queryEl.innerText = inputEl.value;
+      id.innerText = findVal.id;
+      code.innerText = findVal.code;
+      loc.innerText = findVal.location;
+      type.innerText = findVal.type;
+      dist.innerText = findVal.district;
+    }
   }
 };
 
@@ -66,7 +88,27 @@ function spinnerActive() {
   let hidden = spinner.classList.contains("d-none");
   if (hidden) {
     spinner.classList.remove("d-none");
+    queryEl.parentElement.parentElement.classList.add("d-none");
   } else {
     spinner.classList.add("d-none");
   }
 }
+// Alert function ( Manually trigger the function )
+const alertMsg = (title, type, message) => {
+  // creating new Element
+  const alertEl = document.createElement("div");
+
+  alertEl.innerHTML = `
+  <div
+    class="alert alert-${type} alert-dismissible fade show"
+    role="alert">
+    <strong>${title}</strong> ${message}.
+    <button
+      type="button"
+      class="btn-close shadow-none"
+      data-bs-dismiss="alert"
+      aria-label="Close"></button>
+  </div>`;
+
+  alertSec.appendChild(alertEl);
+};
