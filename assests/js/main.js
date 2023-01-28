f1b992a90aade75e5ad721c80fb0c7eb8f97acd5
LP-user1 <akashbhuvi3@gmail.com>
2023-01-28 23:19:18 +0530

    Feature : Added more states & Improve search

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,3 +1,4 @@
+const cYear = document.getElementById("currentYear");
 const inputEl = document.querySelector("input");
 const queryEl = document.querySelector("#query");
 const errorEl = document.querySelector(".not-found");
@@ -10,19 +11,29 @@ const btn = document.querySelector("button");
 const spinner = document.querySelector(".spin");
 const alertSec = document.getElementById("alert-sec");
 const tableEl = document.querySelector(".t-data");
-const cYear = document.getElementById("currentYear");
+const queryTrim = document.getElementById("queryTrim");
+const totalCount = document.getElementById("totalCount");
+const badgeBtn = document.getElementById("badgeBtn");
+const listCount = document.querySelector(".count");
 
 const year = new Date().getFullYear();
 cYear.innerText = year;
 
+if (badgeBtn) {
+  badgeBtn.addEventListener("click", () => {
+    inputEl.value = inputEl.value.slice(0, 2);
+    findRecord();
+  });
+}
+
 inputEl.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
-    findSeperateRecord();
+    findRecord();
   }
 });
-btn.addEventListener("click", findSeperateRecord);
+btn.addEventListener("click", findRecord);
 
-function findSeperateRecord() {
+function findRecord() {
   if (inputEl.value.length > 3) {
     spinnerActive();
     searchRec(inputEl.value);
@@ -60,19 +71,49 @@ const searchRec = async (value) => {
         document.querySelector(".singleData").classList.add("d-none");
         document.querySelector(".allData").classList.remove("d-none");
         queryEl.innerText = inputEl.value;
+        showHideBadgeBtn();
+        alertSec.innerHTML = " ";
         tableEl.innerHTML = " ";
+        listCount.innerText = filterVal.length;
         for (i = 0; i < filterVal.length; i++) {
           const tableVal = document.createElement("tr");
+          tableVal.title = `${filterVal[i].location}`;
+          tableVal.dataset.code = `${filterVal[i].code}`;
           tableVal.innerHTML += `
               <td>${filterVal[i].state}</td>
               <td>${filterVal[i].code}</td>
               <td>${filterVal[i].location}</td>`;
           tableEl.appendChild(tableVal);
         }
+        tableEl.childNodes.forEach((el) => {
+          el.addEventListener("click", (e) => {
+            const RTOCode = e.target.parentElement.dataset.code;
+            inputEl.value = RTOCode;
+            findRecord();
+          });
+        });
       }
     };
     filterExecution();
   } else {
+    let count;
+    count = jsonData.filter((record) => {
+      return record.code.slice(0, 2) === value.toUpperCase().slice(0, 2);
+    });
+    if (count.length >= 100) {
+      totalCount.innerText = "99+";
+    } else if (count.length >= 90) {
+      totalCount.innerText = "89+";
+    } else if (count.length >= 60) {
+      totalCount.innerText = "59+";
+    } else if (count.length >= 50) {
+      totalCount.innerText = "49+";
+    } else if (count.length >= 20) {
+      totalCount.innerText = "19+";
+    } else {
+      totalCount.innerText = "10+";
+    }
+
     const findVal = jsonData.find(
       (record) => record.code === value.toUpperCase()
     );
@@ -88,15 +129,34 @@ const searchRec = async (value) => {
       queryEl.parentElement.parentElement.classList.remove("d-none");
       document.querySelector(".allData").classList.add("d-none");
       document.querySelector(".singleData").classList.remove("d-none");
+      alertSec.innerHTML = " ";
+      showHideBadgeBtn();
+      totalCount.parentElement.title =
+        "All list for " + inputEl.value.toUpperCase().slice(0, 2);
       queryEl.innerText = inputEl.value;
       id.innerText = findVal.id;
       code.innerText = findVal.code;
+      code.parentElement.title = "code: " + findVal.code;
       loc.innerText = findVal.location;
+      loc.parentElement.title = "location: " + findVal.location;
       type.innerText = findVal.type;
+      type.parentElement.title = "type: " + findVal.type;
       dist.innerText = findVal.district;
+      dist.parentElement.title = "district: " + findVal.district;
     }
   }
 };
+
+function showHideBadgeBtn() {
+  if (inputEl.value.length > 3) {
+    listCount.parentElement.classList.add("d-none");
+    queryTrim.parentElement.classList.remove("d-none");
+    queryTrim.innerText = inputEl.value.slice(0, 2);
+  } else {
+    listCount.parentElement.classList.remove("d-none");
+    queryTrim.parentElement.classList.add("d-none");
+  }
+}
 
 function spinnerActive() {
   let hidden = spinner.classList.contains("d-none");
@@ -109,6 +169,7 @@ function spinnerActive() {
 }
 // Alert function ( Manually trigger the function )
 const alertMsg = (title, type, message) => {
+  alertSec.innerHTML = " ";
   // creating new Element
   const alertEl = document.createElement("div");
 
